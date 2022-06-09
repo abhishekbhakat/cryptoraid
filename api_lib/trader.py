eb44af9c068bac1032d66133824d7da720a2de8a
abhishekbhakat <abhishek.bhakat@hotmail.com>
2022-06-09 14:36:16 +0530

    trader class inherited for different brokers

--- api_lib/trader.py
+++ api_lib/trader.py
@@ -2,6 +2,7 @@ from api_lib.wallet import Wallet
 import configparser
 import hmac, json, hashlib, time
 from api_lib.dcx_requests import dcx_get, dcx_post
+from api_lib.binance_requests import binance_get, binance_post
 import logging
 import sys
 from api_lib.precision import round_down, round_up
@@ -20,39 +21,14 @@ class Trader:
         self.paddle = 0
         self.init_wallet()
 
-    def xauth_sign(self, json_body: json) -> str:
-        secret_bytes = bytes(self.secret, encoding="utf-8")
-        signature = hmac.new(
-            secret_bytes, json_body.encode(), hashlib.sha256
-        ).hexdigest()
-        return signature
+    def ticker(self) -> float:
+        return 0.0
 
-    def ticker(self) -> dict:
-        ticker_url = (
-            self.config["public"]["public_base_url"] + self.config["public"]["ticker"]
-        )
-        response = dcx_get(ticker_url)
-        ticker_id = -1
-        for i in range(len(response)):
-            if (
-                response[i]["market"]
-                == self.config["main"]["target_symbol"]
-                + self.config["main"]["source_symbol"]
-            ):
-                ticker_id = i
-                break
-        if ticker_id < 0:
-            logging.error(
-                "Invalid Target symbol and Source symbol. Check configurations."
-            )
-            sys.exit()
-        for key in response[ticker_id]:
-            try:
-                response[ticker_id][key] = float(response[ticker_id][key])
-            except:
-                logging.debug("Non float value.")
-        logging.debug("Ticker : {}".format(response[ticker_id]["last_price"]))
-        return response[ticker_id]
+    def init_wallet(self) -> None:
+        return
+
+    def update_wallet(self) -> None:
+        return
 
     def falling_trend(self) -> None:
         logging.info(
@@ -92,6 +68,148 @@ class Trader:
             tidy()
             ticker_old = ticker_new
 
+    def profit(self, selling: float) -> float:
+        ticker = self.ticker()
+        profit = (
+            ticker["last_price"] * selling
+            - self.paddle
+            - self.paddle * float(self.config["main"]["fee"]) / 100
+        )
+        return profit
+
+    def sudo_profit(self, prev_qty: float, prev_ticker: float) -> float:
+        sudo_ticker = (
+            prev_qty * prev_ticker
+            - float(self.config["main"]["profit"])
+            - self.paddle * float(self.config["main"]["fee"]) / 100
+        ) / prev_qty
+        return sudo_ticker
+
+    def buy(self, qty: float, price: float) -> None:
+        return
+
+    def sell(self, qty: float, price: float) -> None:
+        return
+
+
+class DCX_Trader(Trader):
+    def __init__(self, configpath: dict) -> None:
+        super().__init__(configpath)
+        self.wallet = Wallet()
+        logging.info("Creating a Wallet for trading agent.")
+        self.init_wallet()
+
+    def xauth_sign(self, json_body: json) -> str:
+        secret_bytes = bytes(self.secret, encoding="utf-8")
+        signature = hmac.new(
+            secret_bytes, json_body.encode(), hashlib.sha256
+        ).hexdigest()
+        return signature
+
+    def ticker(self) -> float:
+        ticker_url = (
+            self.config["public"]["public_base_url"] + self.config["public"]["ticker"]
+        )
+        response = dcx_get(ticker_url)
+        ticker_id = -1
+        for i in range(len(response)):
+            if (
+                response[i]["market"]
+                == self.config["main"]["target_symbol"]
+                + self.config["main"]["source_symbol"]
+            ):
+                ticker_id = i
+                break
+        if ticker_id < 0:
+            logging.error(
+                "Invalid Target symbol and Source symbol. Check configurations."
+            )
+            sys.exit()
+        for key in response[ticker_id]:
+            try:
+                response[ticker_id][key] = float(response[ticker_id][key])
+            except:
+                logging.debug("Non float value.")
+        logging.debug("Ticker : {}".format(response[ticker_id]["last_price"]))
+        return response[ticker_id]
+
+    def get_order(self, id: int) -> dict:
+        status_url = (
+            self.config["individual"]["base_url"] + self.config["individual"]["status"]
+        )
+        timeStamp = int(round(time.time() * 1000))
+        body = {"id": id, "timestamp": timeStamp}
+        json_body = json.dumps(body, separators=(",", ":"))
+        response = dcx_post(status_url, self.key, self.xauth_sign(json_body), json_body)
+        logging.info("DCX : {}".format(json.dumps(response)))
+        return response
+
+    def buy(self) -> None:
+        buy_url = (
+            self.config["individual"]["base_url"]
+            + self.config["individual"]["create_order"]
+        )
+        timeStamp = int(round(time.time() * 1000))
+        ticker = self.ticker()
+        body = {
+            "side": "buy",  # Toggle between 'buy' or 'sell'.
+            "order_type": "market_order",  # Toggle between a 'market_order' or 'limit_order'.
+            "market": self.config["main"]["target_symbol"]
+            + self.config["main"][
+                "source_symbol"
+            ],  # Replace 'SNTBTC' with your desired market pair.
+            "total_quantity": round_up(
+                self.paddle / ticker["last_price"], 5
+            ),  # Replace this with the quantity you want
+            "timestamp": timeStamp,
+        }
+        json_body = json.dumps(body, separators=(",", ":"))
+        response = dcx_post(buy_url, self.key, self.xauth_sign(json_body), json_body)
+        logging.info("DCX : {}".format(json.dumps(response)))
+        order_id = response["orders"][0]["id"]
+        status = self.get_order(order_id)["status"]
+        while status not in ["filled", "rejected"]:
+            timer(1)
+            status = self.get_order(order_id)["status"]
+        if status == "rejected":
+            self.buy()
+            return
+        logging.info("BUY : {}".format(order_id))
+        return self.get_order(order_id)
+
+    def sell(self, quantity: float) -> dict:
+        sell_url = (
+            self.config["individual"]["base_url"]
+            + self.config["individual"]["create_order"]
+        )
+        timeStamp = int(round(time.time() * 1000))
+        ticker = self.ticker()
+        body = {
+            "side": "sell",  # Toggle between 'buy' or 'sell'.
+            "order_type": "market_order",  # Toggle between a 'market_order' or 'limit_order'.
+            "market": self.config["main"]["target_symbol"]
+            + self.config["main"][
+                "source_symbol"
+            ],  # Replace 'SNTBTC' with your desired market pair.
+            "total_quantity": round_down(
+                quantity, 5
+            ),  # Replace this with the quantity you want
+            "timestamp": timeStamp,
+        }
+        json_body = json.dumps(body, separators=(",", ":"))
+        response = dcx_post(sell_url, self.key, self.xauth_sign(json_body), json_body)
+        logging.info("DCX : {}".format(json.dumps(response)))
+        order_id = response["orders"][0]["id"]
+        status = self.get_order(order_id)["status"]
+        while status not in ["filled", "rejected"]:
+            timer(1)
+            status = self.get_order(order_id)["status"]
+        if status == "rejected":
+            self.sell()
+            return
+        logging.info("SELL : {}".format(order_id))
+        return self.get_order(order_id)
+
     def init_wallet(self) -> None:
         balance_url = (
             self.config["individual"]["base_url"]
@@ -122,7 +240,6 @@ class Trader:
             "Decide the paddle source_amount. (Suggested to refer previous transactions)"
         )
         ticker = self.ticker()
-        # pprint(ticker)
         logging.info(
             "Estimated source funds = {}".format(
                 self.wallet.target_balance["balance"] * ticker["last_price"]
@@ -263,96 +380,41 @@ class Trader:
                     self.wallet.source_balance["balance"]
                 )
 
-    def get_order(self, id: int) -> dict:
-        status_url = (
-            self.config["individual"]["base_url"] + self.config["individual"]["status"]
+
+class Binance_Trader(Trader):
+    def __init__(self, configpath: dict) -> None:
+        super().__init__(configpath)
+        self.config
+        self.key = self.config["binance"]["api_key"]
+        self.secret = self.config["binance"]["api_secret"]
+        self.symbol = (
+            self.config["main"]["target_symbol"] + self.config["main"]["source_symbol"]
+        )
+        logging.info("Creating a Wallet for trading agent.")
+        self.init_wallet()
+
+    def ticker(self) -> float:
+        url = self.conf["binance"]["base_url"] + self.conf["binance"]["ticker"]
+        key = self.conf["binance"]["api_key"]
+        params = {"symbol": self.symbol}
+        return float(binance_get(url, key, params=params).get("price"))
+
+    def init_wallet(self) -> None:
+        balance_url = (
+            self.config["binance"]["base_url"] + self.config["binance"]["balances"]
         )
         timeStamp = int(round(time.time() * 1000))
-        body = {"id": id, "timestamp": timeStamp}
-        json_body = json.dumps(body, separators=(",", ":"))
-        response = dcx_post(status_url, self.key, self.xauth_sign(json_body), json_body)
-        logging.info("DCX : {}".format(json.dumps(response)))
-        return response
+        body = {"timestamp": timeStamp}
+        
 
-    def buy(self) -> None:
-        buy_url = (
-            self.config["individual"]["base_url"]
-            + self.config["individual"]["create_order"]
-        )
-        timeStamp = int(round(time.time() * 1000))
-        ticker = self.ticker()
-        body = {
-            "side": "buy",  # Toggle between 'buy' or 'sell'.
-            "order_type": "market_order",  # Toggle between a 'market_order' or 'limit_order'.
-            "market": self.config["main"]["target_symbol"]
-            + self.config["main"][
-                "source_symbol"
-            ],  # Replace 'SNTBTC' with your desired market pair.
-            "total_quantity": round_up(
-                self.paddle / ticker["last_price"], 5
-            ),  # Replace this with the quantity you want
-            "timestamp": timeStamp,
-        }
-        json_body = json.dumps(body, separators=(",", ":"))
-        response = dcx_post(buy_url, self.key, self.xauth_sign(json_body), json_body)
-        logging.info("DCX : {}".format(json.dumps(response)))
-        order_id = response["orders"][0]["id"]
-        status = self.get_order(order_id)["status"]
-        while status not in ["filled", "rejected"]:
-            timer(1)
-            status = self.get_order(order_id)["status"]
-        if status == "rejected":
-            self.buy()
-            return
-        logging.info("BUY : {}".format(order_id))
-        return self.get_order(order_id)
+    def update_wallet(self) -> None:
+        return
 
-    def sell(self, quantity: float) -> dict:
-        sell_url = (
-            self.config["individual"]["base_url"]
-            + self.config["individual"]["create_order"]
-        )
-        timeStamp = int(round(time.time() * 1000))
-        ticker = self.ticker()
-        body = {
-            "side": "sell",  # Toggle between 'buy' or 'sell'.
-            "order_type": "market_order",  # Toggle between a 'market_order' or 'limit_order'.
-            "market": self.config["main"]["target_symbol"]
-            + self.config["main"][
-                "source_symbol"
-            ],  # Replace 'SNTBTC' with your desired market pair.
-            "total_quantity": round_down(
-                quantity, 5
-            ),  # Replace this with the quantity you want
-            "timestamp": timeStamp,
-        }
-        json_body = json.dumps(body, separators=(",", ":"))
-        response = dcx_post(sell_url, self.key, self.xauth_sign(json_body), json_body)
-        logging.info("DCX : {}".format(json.dumps(response)))
-        order_id = response["orders"][0]["id"]
-        status = self.get_order(order_id)["status"]
-        while status not in ["filled", "rejected"]:
-            timer(1)
-            status = self.get_order(order_id)["status"]
-        if status == "rejected":
-            self.sell()
-            return
-        logging.info("SELL : {}".format(order_id))
-        return self.get_order(order_id)
+    def buy(self, amount: float) -> None:
+        pass
 
-    def profit(self, selling: float) -> float:
-        ticker = self.ticker()
-        profit = (
-            ticker["last_price"] * selling
-            - self.paddle
-            - self.paddle * float(self.config["main"]["fee"]) / 100
-        )
-        return profit
+    def sell(self, amount: float) -> None:
+        pass
 
-    def sudo_profit(self, prev_qty: float, prev_ticker: float) -> float:
-        sudo_ticker = (
-            prev_qty * prev_ticker
-            - float(self.config["main"]["profit"])
-            - self.paddle * float(self.config["main"]["fee"]) / 100
-        ) / prev_qty
-        return sudo_ticker
+    def get_order(self, order_id: str) -> None:
+        pass
