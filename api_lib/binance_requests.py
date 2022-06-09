ab0f5bd8bba439314736829d1b8e574ecf1957a1
abhishekbhakat <abhishek.bhakat@hotmail.com>
2022-06-09 14:35:36 +0530

    binance special body as params

--- api_lib/binance_requests.py
+++ api_lib/binance_requests.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import logging
+import hmac, hashlib
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +21,7 @@ def binance_post(
         response = requests.request(
             "POST", url, data=body, headers=headers, params=params
         )
-        if not response.status_code == 200:
+        if not response.status_code >= 200 and response.status_code < 300:
             log.debug("RESPONSE : " + str(response.status_code))
             log.warning("CHECK CONNECTION")
             time.sleep(5)
@@ -32,6 +33,13 @@ def binance_post(
         return binance_post(url, key, signature, body)
 
 
+def body_to_params(body: dict) -> str:
+    params = ""
+    for key, value in body.items():
+        params += key + "=" + str(value) + "&"
+    return params[:-1]
+
+
 def binance_get(url: str, key: str, body: dict = {}, params: dict = {}) -> dict:
     log.debug("URL: " + url)
     try:
@@ -40,16 +48,29 @@ def binance_get(url: str, key: str, body: dict = {}, params: dict = {}) -> dict:
             "Accept": "application/json",
             "X-MBX-APIKEY": key,
         }
+        # convert body to string
+        payload = body_to_params(body)
+        log.debug("BODY: " + payload)
+        signature = hmac.new(
+            key.encode(), payload.encode(), hashlib.sha256
+        ).hexdigest()
+        payload = payload + "&signature=" + signature
+        log.debug("PAYLOAD: " + payload)
+        input()
         response = requests.request(
-            "GET", url, headers=headers, data=body, params=params
+            "GET", url, headers=headers, data=payload, params=params
         )
-        if not response.status_code == 200:
+        if not response.status_code >= 200 and response.status_code < 300:
             log.debug("RESPONSE : " + str(response.status_code))
+            log.debug("RESPONSE: " + str(response.text))
             log.warning("CHECK CONNECTION")
             time.sleep(5)
-            return binance_get(url, key)
+            return binance_get(url, key, body, params)
         return response.json()
     except Exception as e:
-        log.error("ERROR: " + e)
+        log.error("ERROR: " + str(e))
         time.sleep(1)
-        return binance_get(url, key)
+        return binance_get(url, key, body, params)
+
+
+# "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559" | openssl dgst -sha256 -hmac "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
